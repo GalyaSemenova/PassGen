283aafb6802acffb3521552d088e82fd0290a3af
GalyaSemenova <73016956+GalyaSemenova@users.noreply.github.com>
2020-10-31 10:14:47 +0600

    Update Password.java

--- Password.java
+++ Password.java
@@ -1,17 +1,19 @@
 public class Password {
 
+    static String symbols = "0123456789_+=*/?!@#&abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static int numOfSymbols = symbols.length();
+
     public static String generate(int length) {
         String password = "";
 
         for (int i = 0; i < length; i++) {
-            password = password + randomSymbol("0123456789_+=*/?!@#&abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            password = password + randomSymbol();
 
         }
         return password;
     }
 
-    public static String randomSymbol(String symbols) {
-        int numOfSymbols = symbols.length();
+    public static String randomSymbol() {
         int r = (int) (numOfSymbols * Math.random());
         return symbols.substring(r, r + 1);
     }
